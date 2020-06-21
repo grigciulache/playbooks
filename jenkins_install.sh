da933116ffdeb23eed1d86289b79053ae7888eed
grigciulache <grigciulache@gmail.com>
2020-06-21 16:22:41 +0000

    added scripts

---
+++ jenkins_install.sh
@@ -0,0 +1,6 @@
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins service
